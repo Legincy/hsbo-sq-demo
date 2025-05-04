462b446abfddfceddb160c3b34a38a69ed4f7386
Martin Peth <martin@peth.pl>
2025-05-04 08:45:50 +0200

    update(action): changed from postgres to h2 while running workflow tests

--- src/test/java/com/example/hsbo_sq_demo/HsboSqDemoApplicationTests.java
+++ src/test/java/com/example/hsbo_sq_demo/HsboSqDemoApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.hsbo_sq_demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class HsboSqDemoApplicationTests {
 
 	@Test
