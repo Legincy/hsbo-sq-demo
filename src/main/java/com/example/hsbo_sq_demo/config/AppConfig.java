1c3216c0dd0f96b0d3c497b1ae1de52a89a24c40
Martin Peth <martin@peth.pl>
2025-06-11 08:32:06 +0200

    feat(metrics): added influx metric config

---
+++ src/main/java/com/example/hsbo_sq_demo/config/AppConfig.java
@@ -0,0 +1,24 @@
+package com.example.hsbo_sq_demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+    /**
+     * This method configures CORS (Cross-Origin Resource Sharing) settings for the application.
+     * It allows requests from "http://localhost:3000" and specifies the allowed HTTP methods,
+     * headers, and credentials.
+     *
+     * @param registry the CORS registry to configure
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://localhost:3001")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
